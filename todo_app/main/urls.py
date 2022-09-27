56265c1a7be252b473319c5e44144a19d500f307
Mariyan Georgiev <mariqn5000@gmail.com>
2022-09-27 12:16:01 +0300

    added login & logout

--- todo_app/main/urls.py
+++ todo_app/main/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 from .views import views as views
 from . import views
@@ -8,4 +9,7 @@ urlpatterns = [
     path('tasks-create', views.CreateTaskView.as_view(), name='task-create'),
     path('tasks-edit/<int:pk>', views.EditTaskView.as_view(), name='task-edit'),
     path('tasks-delete/<int:pk>', views.DeleteTaskView.as_view(), name='task-delete'),
+
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout')
+    path('login/', views.LoginView.as_view(), name='login')
 ]
--- todo_app/main/views.py
+++ todo_app/main/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import reverse_lazy
@@ -36,5 +37,15 @@ class EditTaskView(views.UpdateView):
 
 class DeleteTaskView(views.DeleteView):
     model = Task
-    template_name = 'task_delete.html'
     success_url = reverse_lazy('tasks')
+
+
+class LoginView(auth_views.LoginView):
+    template_name = 'login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    success_url = reverse_lazy('tasks')
+
+class LogoutView(auth_views.LogoutView):
+    pass
